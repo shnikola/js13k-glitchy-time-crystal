ac482e680a7a603d2a30ebbe04d7f6601bba22b4
Nikola <anrakee@gmail.com>
2015-08-28 01:24:04 +0200

    Showing some muthafuckin squares!

--- game/src/client.js
+++ game/src/client.js
@@ -1,17 +1,63 @@
-function() {
-  
+(function() {
+
+var canvas = document.getElementById("world");
+var context = canvas.getContext("2d");
+canvas.width = 640;
+canvas.height = 480;
+
+window.requestAnimationFrame =
+  window.requestAnimationFrame ||
+  window.mozRequestAnimationFrame ||
+  window.webkitRequestAnimationFrame ||
+  window.msRequestAnimationFrame ||
+  function(fn){ setTimeout(fn, 33); };
+
+var connected = false;
+var socket = {};
+
+var players = [];
+
 init();
 
-var world = {
-  width: 640,
-  height: 480,
-  x: 0,
-  y: 0
-};
-
 function init() {
-  
+  connect();
+  setTimeout(tic, 10);
+}
+
+function connect() {
+  connected = true;
+  console.log('connecting');
+  if (!socket.connected) socket = io(document.location.href);
+  socket.on('update', onUpdate);
+  socket.on('config', onConfig);
+  socket.on('disconnect', onDisconnect);
+  // socket.emit('playerInfo', { name: playerName });
+}
+
+
+function onUpdate(data) {
+  players = [];
+  players = players.concat(data.aTeam);
+  players = players.concat(data.bTeam);
+}
+
+function onConfig() {}
+function onDisconnect() {}
+
+function tic() {
+  console.log(players)
+  for (var i = 0; i < players.length; i++) {
+    drawPlayer(players[i]);
+  }
+  window.requestAnimationFrame(tic);
+}
+
+function drawPlayer(sprite) {
+  console.log(sprite)
+  context.fillStyle = "rgb(255, 255, 255)";
+  context.fillRect(sprite.x, sprite.y, 10, 10);
 }
 
   
-}();
+})();
+
--- game/src/server.js
+++ game/src/server.js
@@ -3,8 +3,61 @@ log('Hi! This is some game.');
 var socketio = require('sandbox-io');
 log('Loaded sandbox-io', socketio);
 
-socketio.on('connection', function(socket){
+// TODO: better game managemt
+var game = new Game();
+
+socketio.on('connection', function(socket) {
+  if (!game.running) game.start();
+  log.debug('New connection', socket.id);
+  
+  player = new Player();
+  player.joinGame(game)
+  
   socket.on('event', function(data){});
   socket.on('disconnect', function(){});
 });
 
+
+// ------ Game State -------
+
+function Game() {
+  this.id = 'game' + Math.random();
+  this.world = {
+    width: 640,
+    height: 480
+  };
+
+  this.aTeam = [];
+  this.bTeam = [];
+  this.bullets = [];
+}
+
+Game.prototype.start = function() {
+  this.running = true;
+  setTimeout(this.tic.bind(this), 1000);
+}
+
+Game.prototype.tic = function() {
+  socketio.emit('update', {
+    aTeam: this.aTeam,
+    bTeam: this.bTeam,
+    bullets: this.bullets
+  });
+  setTimeout(this.tic.bind(this), 33);
+}
+
+// ------ Player -------
+
+function Player(socket) {
+  this.socket = socket;
+  // this.name = 'player' + pCounter++;
+  // socket.on('playerInfo', this.onPlayerInfo.bind(this));
+  // socket.on('disconnect', this.onExit.bind(this));
+}
+
+Player.prototype.joinGame = function(game) {
+  this.team = game.aTeam.length <= game.bTeam.length ? 'a' : 'b';
+  game[this.team + 'Team'].push(this);
+  this.x = game.world.width * Math.random();
+  this.y = game.world.height * Math.random();
+}
